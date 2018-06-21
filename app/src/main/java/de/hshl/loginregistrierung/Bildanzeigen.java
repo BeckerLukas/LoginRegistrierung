f19e9cf33953cb01675cfcbf8b30fce31e689322
Lukas Becker <Lukas.Becker@stud.hshl.de>
2018-06-21 12:56:48 +0200

    Bild anzeigen

---
+++ app/src/main/java/de/hshl/loginregistrierung/Bildanzeigen.java
@@ -0,0 +1,13 @@
+package de.hshl.loginregistrierung;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Bildanzeigen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bildanzeigen);
+    }
+}
